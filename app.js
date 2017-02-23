6d18e404aaf9771c929a7b2754eb659d23d30097
Morgan Fogarty <mofo37@gmail.com>
2017-02-23 14:03:09 -0800

    styled page, created variables

---
+++ app.js
@@ -0,0 +1,4 @@
+var inches = Document.getElementById('inches');
+var miles = Document.getElementById('miles');
+var pounds = Document.getElementById('pounds');
+var shitTon = Document.getElementById('shit-ton');
